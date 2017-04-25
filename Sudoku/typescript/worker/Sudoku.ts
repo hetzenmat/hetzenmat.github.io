ce2abb86e5567bd4b54921098c151d8646673c09
Matthias Hetzenberger <mhetzenberger@gmail.com>
2017-04-25 08:24:45 +0100

    implement sudoku parsing method

--- Sudoku/typescript/worker/Sudoku.ts
+++ Sudoku/typescript/worker/Sudoku.ts
@@ -16,7 +16,36 @@ class Sudoku {
         }
     }
 
+    parse(input: string): void {
+        this.clear();
+        let rows = input.trim().split('\n');
+
+        if (rows.length !== 9) {
+            throw new Error(`9 rows required (${rows.length} given).`);
+        }
+
+        for (let row = 0; row < 9; row++) {
+            if (rows[row].length < 9) {
+                throw new Error(`Length of row ${row + 1} needs to be at least 9 characters (${rows[row].length} given).`);
+            }
+
+            for (let col = 0; col < 9; col++) {
+                let value = parseInt(rows[row][col]);
+
+                if (!this.validNumber(row, col, value)) {
+                    throw new Error(`Number in row ${row + 1} and column ${col + 1} cannot be placed, because it does not satisfy the Sudoku constraints.`);
+                }
+
+                this.grid[row][col] = value;
+            }
+        }
+    }
+
     validNumber(row: number, col: number, value: number): boolean {
+
+        if (isNaN(value) || value <= 0 || value > 9)
+            return false;
+
         // check the row and the column
         for (let i = 0; i < 9; i++) {
             if (this.grid[row][i] === value)
