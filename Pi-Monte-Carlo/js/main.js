f3acba87107817a0fce56ca13557f7d02d4882cd
Matthias Hetzenberger <mhetzenberger@gmail.com>
2017-07-22 12:39:44 +0200

    Finish Monte Carlo Estimation of Pi

--- Pi-Monte-Carlo/js/main.js
+++ Pi-Monte-Carlo/js/main.js
@@ -6,34 +6,44 @@ let resizeTimeout;
 let dropInterval;
 let canvas;
 let context;
+let content;
+let drops = {
+    in: 0,
+    total: 0
+};
+let pi;
 function random(min, max) {
     return Math.random() * (max - min) + min;
 }
-function resizeCanvas() {
-    canvas.height = canvas.width;
-}
 function documentReady() {
+    content = document.getElementById('content');
+    pi = document.getElementById('pi');
     canvas = document.getElementById('canvas');
     context = canvas.getContext('2d');
-    resizeCanvas();
     context.beginPath();
     context.arc(0, 0, canvas.width, 0, 1.5 * Math.PI);
     context.strokeStyle = 'black';
     context.stroke();
-    window.onresize = () => {
-        clearTimeout(resizeTimeout);
-        resizeTimeout = setTimeout(resizeCanvas, 100);
-    };
-    dropInterval = setInterval(next_drop, 25);
+    dropInterval = setInterval(next_drops, 50);
 }
-function next_drop() {
-    let x = random(0, canvas.width);
-    let y = random(0, canvas.height);
-    if (x * x + y * y <= canvas.width * canvas.width) {
-        context.fillStyle = "blue";
+function next_drops() {
+    for (let i = 0; i < 10; i++) {
+        let x = random(0, canvas.width);
+        let y = random(0, canvas.height);
+        if (x * x + y * y <= canvas.width * canvas.width) {
+            context.fillStyle = "green";
+            drops.in++;
+        }
+        else {
+            context.fillStyle = "red";
+        }
+        drops.total++;
+        context.fillRect(x, y, 1, 1);
+        let approximation = (4 * drops.in / drops.total).toString();
+        while (approximation.length < 12) {
+            approximation += '0';
+        }
+        approximation = approximation.substring(0, 13);
+        pi.innerHTML = approximation;
     }
-    else {
-        context.fillStyle = "red";
-    }
-    context.fillRect(x, y, 1, 1);
 }
--- Pi-Monte-Carlo/typescript/main.ts
+++ Pi-Monte-Carlo/typescript/main.ts
@@ -7,21 +7,25 @@ let resizeTimeout: number;
 let dropInterval: number;
 let canvas: HTMLCanvasElement;
 let context: CanvasRenderingContext2D;
+let content: HTMLDivElement;
+let drops = {
+    in: 0,
+    total: 0
+};
+let pi: HTMLParagraphElement;
+
 
 function random(min: number, max: number): number {
   return Math.random() * (max - min) + min;
 }
 
-function resizeCanvas(): void {
-    canvas.height = canvas.width;
-}
-
 function documentReady(): void {
+
+    content = <HTMLDivElement>document.getElementById('content');
+    pi = <HTMLParagraphElement>document.getElementById('pi');
 
     canvas = <HTMLCanvasElement>document.getElementById('canvas');
     context = canvas.getContext('2d');
-
-    resizeCanvas();
 
     context.beginPath();
 
@@ -30,23 +34,34 @@ function documentReady(): void {
     context.strokeStyle = 'black';
     context.stroke();
 
-    window.onresize = () => {
-        clearTimeout(resizeTimeout);
-        resizeTimeout = setTimeout(resizeCanvas, 100);
-    }
-
-    dropInterval = setInterval(next_drop, 25);
+    dropInterval = setInterval(next_drops, 50);
 }
 
-function next_drop(): void {
-    let x = random(0, canvas.width);
-    let y = random(0, canvas.height);
+function next_drops(): void {
 
-    if (x*x + y*y <= canvas.width*canvas.width) {
-        context.fillStyle = "blue";
-    } else {
-        context.fillStyle = "red";
+    for (let i = 0; i < 10; i++) {
+        let x = random(0, canvas.width);
+        let y = random(0, canvas.height);
+
+        if (x*x + y*y <= canvas.width*canvas.width) {
+            context.fillStyle = "green";
+            drops.in++;
+        } else {
+            context.fillStyle = "red";    
+        }
+
+        drops.total++;
+
+        context.fillRect(x, y, 1, 1);
+
+        let approximation = (4 * drops.in / drops.total).toString();
+
+        // fix approximation to 10 decimal places
+        while (approximation.length < 12) {
+            approximation += '0';
+        }
+        approximation = approximation.substring(0, 13);
+
+        pi.innerHTML = approximation;
     }
-
-    context.fillRect(x, y, 1, 1);
 }
